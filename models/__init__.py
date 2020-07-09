1bc5a7aafd27c611f8502b152fbe228037ef8c61
Alluka <hsiaoyeung.lee@gmail.com>
2020-07-09 22:28:44 +0800

    studentnet

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/9 10:27 下午
+# @Author  : Sean Lee
+# @Email   : 13382092340@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+
